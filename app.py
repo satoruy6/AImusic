b058ac597dce14204cfd77f048db1805da9b13af
satoruy6 <48147208+satoruy6@users.noreply.github.com>
2023-02-03 19:51:45 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ subprocess.run(["apt", "install", "fluid-soundfont-gm"])
 subprocess.run(["apt", "install", "build-essential"])
 subprocess.run(["apt", "install", "libasound2-dev"])
 subprocess.run(["apt", "install", "libjack-dev"])
+subprocess.run(["apt-get", "install", "libsndfile1"])
 import os
 os.system("gsutil -q -m cp gs://download.magenta.tensorflow.org/models/music_vae/multitrack/* /content/")
 #import setuptools
