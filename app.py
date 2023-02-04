dc6343d55846bae02bfdb8d4a465f9970c0b4798
satoruy6 <48147208+satoruy6@users.noreply.github.com>
2023-02-04 21:00:40 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import os
 os.system("python -m pip install --upgrade pip")
-#os.system("curl https://sdk.cloud.google.com | bash")
-#os.system("gcloud init")
-#os.system("gsutil -q -m cp gs://download.magenta.tensorflow.org/models/music_vae/multitrack/* /content/")
-os.system("mkdir /content/")
-os.system("cd /content/")
-os.system("wget gs://download.magenta.tensorflow.org/models/music_vae/multitrack/*")
+os.system("gsutil config")
+os.system("gsutil -q -m cp gs://download.magenta.tensorflow.org/models/music_vae/multitrack/* /content/")
 
 import streamlit as st
 st.set_page_config(page_title="AImusic app")
